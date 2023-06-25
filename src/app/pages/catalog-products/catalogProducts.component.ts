83aa9d2079bcf16a6578409c4f47028663661b63
Camilo Guio <camilo.guio@linktic.com>
2023-06-25 12:51:19 -0500

    Add store import component

--- src/app/pages/catalog-products/catalogProducts.component.ts
+++ src/app/pages/catalog-products/catalogProducts.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Store } from "@ngrx/store";
+import { AppState } from "../../app.reducer";
 
 @Component({
   selector: 'app-catalog-products',
   templateUrl: './catalogProducts.component.html',
   styleUrls: ['./catalogProducts.component.scss']
 })
-export class CatalogProductsComponent {
+export class CatalogProductsComponent implements OnInit {
 
 }
